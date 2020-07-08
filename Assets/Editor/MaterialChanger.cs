15bb688755ede1ee110d954e867f330f0cab0548
Alex <afriedman1991@gmail.com>
2020-07-08 15:51:44 -0400

    Completed jump animation with raycast

--- Assets/Editor/MaterialChanger.cs
+++ Assets/Editor/MaterialChanger.cs
@@ -1,23 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEditor;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEditor;
 
-namespace roundbeargames_tutorial
-{
-    [CustomEditor(typeof(CharacterControl))]
-    public class MaterialChanger : Editor
-    {
-        public override void OnInspectorGUI()
-        {
-            DrawDefaultInspector();
+//namespace roundbeargames_tutorial
+//{
+//    [CustomEditor(typeof(CharacterControl))]
+//    public class MaterialChanger : Editor
+//    {
+//        public override void OnInspectorGUI()
+//        {
+//            DrawDefaultInspector();
             
-            CharacterControl control = (CharacterControl)target;
+//            CharacterControl control = (CharacterControl)target;
 
-            if (GUILayout.Button("Change Material"))
-            {
-                control.ChangeMaterial();
-            }
-        }
-    }
-}
+//            if (GUILayout.Button("Change Material"))
+//            {
+//                control.ChangeMaterial();
+//            }
+//        }
+//    }
+//}
---
+++ Assets/States/StateScripts/GroundDetector.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace roundbeargames_tutorial
+{
+    [CreateAssetMenu(fileName = "New State", menuName = "Roundbeargames/AbilityData/GroundDetector")]
+    public class GroundDetector : StateData
+    {
+        public float Distance;
+
+        public override void OnEnter(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
+        {
+            
+        }
+
+        public override void UpdateAbility(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
+        {
+            CharacterControl control = characterState.GetCharacterControl(animator);
+
+            if (IsGrounded(control))
+            {
+                animator.SetBool(TransitionParameter.Grounded.ToString(), true);
+            }
+            else
+            {
+                animator.SetBool(TransitionParameter.Grounded.ToString(), false);
+            }
+        }
+
+        public override void OnExit(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
+        {
+
+        }
+
+        bool IsGrounded(CharacterControl control)
+        {
+            if (control.RIGID_BODY.velocity.y > -0.01f && control.RIGID_BODY.velocity.y <= 0f)
+            {
+                return true;
+            }
+
+            foreach(GameObject o in control.BottomSpheres)
+            {
+                Debug.DrawRay(o.transform.position, -Vector3.up * 0.7f, Color.yellow);
+                RaycastHit hit;
+                if (Physics.Raycast(o.transform.position, -Vector3.up, out hit, Distance))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
